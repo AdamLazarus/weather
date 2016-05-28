58177d7d9cf1f763885c9396232db153ab1d59c0
Adam Lazarus <adamlazarus92@gmail.com>
2016-05-28 22:11:39 +0800

    Switched to forecast io API for SSL use

--- js/weather.js
+++ js/weather.js
@@ -12,53 +12,55 @@ function getLocation() {
     function foundLocation(position) {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-      urlpath = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=7b55adeb0d728359168d323b4e66d693";
+      urlpath = "https://api.forecast.io/forecast/15c542bede243bcd7698fb33d5963755/" + lat + "," + long;
       loadWeatherData();
+      getCity();
       }
 
-    function noLocation() {
-        alert('Could not find location');
+    function noLocation(error) {
+        alert(error.message);
     }
 }
 
 function loadWeatherData() {
       $.ajax({
       url: urlpath,
+      type: "POST",
+      dataType: 'jsonp',
      success: function(result){
-     $("#cityName").append(result.name);
-     $("#tempF").append(kToF(result.main.temp));
-     $("#tempC").append(kToC(result.main.temp) + "°C");
-     $("#weatherType").append(result.weather[0].description);
-     var newDataIcon = "";
-     if(result.weather[0].id >= 200 && result.weather[0].id <= 232){
-       newDataIcon = "0";
-     }
-     else if(result.weather[0].id >= 300 && result.weather[0].id <= 321){
-       newDataIcon = "Q";
-     }
-     else if(result.weather[0].id >= 500 && result.weather[0].id <= 531){
-       newDataIcon = "R";
-     }
-     else if(result.weather[0].id >= 600 && result.weather[0].id <= 622){
-       newDataIcon = "W";
-     }
-     else if(result.weather[0].id >= 701 && result.weather[0].id <= 781){
-       newDataIcon = "M";
-     }
-     else if(result.weather[0].id == 800){
-       newDataIcon = "B";
-     }
-     else{
-       newDataIcon = "N";
-     }
+     $("#tempF").append(kToF(result.currently.temperature));
+     $("#tempC").append(fToC(result.currently.temperature) + "°C");
+     $("#weatherType").append(result.currently.summary);
+     var skycons = new Skycons({"color": "black"});
+      skycons.add("icon1", result.currently.icon);
+      skycons.play();
+    },
 
-     $(".weather-icon").attr('data-icon', newDataIcon);},
-
-     error: function(){
-       alert("boo");
+     error: function(error){
+       alert(error.message);
      }
      }
      );
+}
+
+function getCity(){
+  $.ajax({
+  url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyCIOhpHRouUCMTAeLi6OtxrrV3mdxO3w4c",
+  type: "POST",
+  dataType: 'json',
+ success: function(result){
+     $("#cityName").append(result.results[3].formatted_address);
+ },
+
+ error: function(error){
+   alert(error.message);
+ }
+ }
+ );
+}
+
+function fToC(number){
+  return Math.round(((number - 32) / 1.8) * 10)/10;
 }
 
 function kToC(number){
